82e7fc8e5cea659fd0702890b40fca5c2cf7287b
SGiuri <s.giuri@issm.it>
2020-09-19 22:43:23 +0200

    Done

--- grains.py
+++ grains.py
@@ -1,6 +1,9 @@
 def square(number):
-    pass
+    if 0 < number < 65 and number == int(number):
+        return 2 ** (number - 1)
+
+    raise ValueError("Not positive natural number")
 
 
 def total():
-    pass
+    return 2 ** (64) - 1
